e0f9f8a5dc56fc2086e63a52fc4e9dccbeda7564
Luis Erhardt <luis@mail.com>
2025-08-07 00:27:50 +0200

    set timezone to Germany

--- scripts/write-build-info.cjs
+++ scripts/write-build-info.cjs
@@ -1,7 +1,9 @@
 const fs = require("fs");
 const path = require("path");
 
-const date = new Date().toLocaleDateString("de-DE");
+const date = new Date().toLocaleDateString("de-DE", {
+  timeZone: "Europe/Berlin",
+});
 const content = `export const BUILD_DATE = '${date}';\n`;
 
 fs.writeFileSync(path.join(__dirname, "../src/build-info.ts"), content);
